169ccbaa5300d11e0681aa99e4325b0b290b062d
Anas Khalifa <anasgets111@gmail.com>
2024-01-18 14:49:01 +0200

    after 1st video

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,44 @@
+const todoList = [{name: 'make diner', dueDate: '2024-12-22'}, {name: 'was dishes', dueDate: '2024-12-15'}];
+renderTodoList ();
+
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+}
+
+function renderTodoList() {
+  let todoListHTML = ``;
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const {name, dueDate} = todoObject;
+    //const dueDate = todoObject.dueDate;
+    const html = `
+    <div> ${name} </div> 
+    <div>  ${dueDate} </div>
+    <button class="delete-todo-button" onclick="todoList.splice(${i}, 1);
+    renderTodoList ();
+    "> Delete </button>
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector('.js-due-date-input')
+  const dueDate = dateInputElement.value
+
+
+  todoList.push({
+    
+    name, dueDate
+    
+  });
+  console.log(todoList);
+  inputElement.value = "";
+  renderTodoList ();
+}
